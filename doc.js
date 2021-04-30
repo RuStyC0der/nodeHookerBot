8d288892c11e3b2ef39284e9a8e22de10fbfa9b9
RuStyC0der <miller_maers@tutanota.com>
2021-04-30 15:26:48 +0000

    typo fix

--- doc.js
+++ doc.js
@@ -8,7 +8,7 @@ const startupMessage = `
 How to use this bot:
 
 First step, set your nickname using:
-/set_nickname <nick>
+/set_nickname <nickmame>
 
 Then you can use hooks, with wget, curl, e.g
 
@@ -17,11 +17,11 @@ n - is your nickname, you can use nickname of another user, then this user will 
 msg - is your message that will be displayed in bot message. 
 
 Example:
-curl "http://${domain}?n=max&msg=test"
+curl "http://${domain}?n=yournickname&msg=test"
 
 Use case:
-bash ./<very_long_task_or_script>.sh && curl "http://${domain}?n=max&msg=task_finished" (curl will be executed only if previous task was finished with 0 status code)
-bash ./<very_long_task_or_script>.sh ; curl "http://${domain}?n=max&msg=task_finished" (curl will be executed even if previous task was finished with non-zero status code)
+bash ./<very_long_task_or_script>.sh && curl "http://${domain}?n=yournickname&msg=task_finished" (curl will be executed only if previous task was finished with 0 status code)
+bash ./<very_long_task_or_script>.sh ; curl "http://${domain}?n=yournickname&msg=task_finished" (curl will be executed even if previous task was finished with non-zero status code)
 
 Note that spaces is not allowed in query parameters, use '+' instead
 
